1878b631b7b04cee8a6975858263be192688d5ea
schultzy0330 <matt.schultz2007@gmail.com>
2026-04-18 02:44:41 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,52 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include <Geode/modify/LevelInfoLayer.hpp>
 #include <Geode/utils/web.hpp>
 #include "ListManager.h"
 #include <string>
+
 using namespace geode::prelude;
 
 class $modify(MenuLayer) {
-
-
-	bool init() {
-		if (!MenuLayer::init()) return false;
-
-		if (ListManager::firstTimeOpen) {
-			return true;
-		}
-web::AsyncWebRequest()
-    .fetch("https://api.aredl.net/api/aredl/list")
-    .text()
-    .then([](std::string const& str) {
-        if (str == "Failed." || str == "-1") {
-            ListManager::firstTimeOpen = true;
-            ListManager::filterType = -2;
-            std::string errorStr = "\n\n<cr>Could not load data from AREDL.</c>\nThe API could be down, but chances are, your internet just sucks.\n\n<cg>Restart your game to try again.</c>\n\n<cb>-Grandpa Demon</c>";
-            FLAlertLayer::create("What the??", str + errorStr, "OK")->show();
-            return;
+    bool init() {
+        if (!MenuLayer::init()) {
+            return false;
         }
 
-        ListManager::parseRequestString(str);
-        ListManager::firstTimeOpen = true;
-        ListManager::filterType = -1;
-    })
-    .expect([](std::string const& error) {
-        ListManager::firstTimeOpen = true;
-        ListManager::filterType = -2;
-        std::string errorStr = "\n\n<cr>Could not load data from AREDL.</c>\nThe API could be down, but chances are, your internet just sucks.\n\n<cg>Restart your game to try again.</c>\n\n<cb>-Grandpa Demon</c>";
-        FLAlertLayer::create("What the??", error + errorStr, "OK")->show();
-    });
-		
+        if (ListManager::firstTimeOpen) {
+            return true;
+        }
 
-		// if (!ListManager::firstTimeOpen) {
-		// 	web::AsyncWebRequest()
-		// 	.fetch("https://api.aredl.net/api/aredl/list")
-		// 	.text()
-		// 	.then([](std::string const& str) {
-		// 		ListManager::parseRequestString(str);
-		// 		ListManager::firstTimeOpen = true;
-		// 		ListManager::filterType = -1;
-		// 	})
-		// 	.expect([](std::string const& error) {
-		// 		ListManager::firstTimeOpen = true;
-		// 		ListManager::filterType = -2;
-		// 		std::string errorStr = "\n\n<cr>Could not load data from AREDL.</c>\nThe API could be down, but chances are, your internet just sucks.\n\n<cg>Restart your game to try again.</c>\n\n<cb>-Grandpa Demon</c>";
-		// 		FLAlertLayer::create("What the??", error + errorStr, "OK")->show();
-		// 	});
-		// }
-		
+        web::AsyncWebRequest()
+            .fetch("https://api.aredl.net/api/aredl/list")
+            .text()
+            .then([](std::string const& str) {
+                if (str == "Failed." || str == "-1") {
+                    ListManager::firstTimeOpen = true;
+                    ListManager::filterType = -2;
+                    std::string errorStr =
+                        "\n\n<cr>Could not load data from AREDL.</c>\n"
+                        "The API could be down, but chances are, your internet just sucks.\n\n"
+                        "<cg>Restart your game to try again.</c>\n\n"
+                        "<cb>-Grandpa Demon</c>";
+                    FLAlertLayer::create("What the??", str + errorStr, "OK")->show();
+                    return;
+                }
 
-		return true;
-	}
+                ListManager::parseRequestString(str);
+                ListManager::firstTimeOpen = true;
+                ListManager::filterType = -1;
+            })
+            .expect([](std::string const& error) {
+                ListManager::firstTimeOpen = true;
+                ListManager::filterType = -2;
+                std::string errorStr =
+                    "\n\n<cr>Could not load data from AREDL.</c>\n"
+                    "The API could be down, but chances are, your internet just sucks.\n\n"
+                    "<cg>Restart your game to try again.</c>\n\n"
+                    "<cb>-Grandpa Demon</c>";
+                FLAlertLayer::create("What the??", error + errorStr, "OK")->show();
+            });
+
+        return true;
+    }
 };
-
-
